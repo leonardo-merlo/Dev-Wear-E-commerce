af3ec0ff64d33f035f8b5ed66117f4569e57b020
leonardo-merlo <leomamerlo@gmail.com>
2025-08-04 15:41:40 -0300

    chore: update shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
-      <h1 className="px-5 text-4xl font-bold">Hello World</h1>
+    <div>
+      <Button>Click me</Button>
     </div>
   );
 }
