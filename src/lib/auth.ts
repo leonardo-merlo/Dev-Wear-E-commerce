fabd61cd2b530fe4aed6a9be31e4d13474264caa
leonardo-merlo <leomamerlo@gmail.com>
2025-08-05 19:30:05 -0300

    feat: add sign in with email and password

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,25 @@
 import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-import { db } from "@/db"
+import { db } from "@/db";
+import * as schema from "@/db/schema";
+
  
 export const auth = betterAuth({
+    emailAndPassword: { 
+        enabled: true, 
+      }, 
     database: drizzleAdapter (db, {
         provider: "pg",
-    })
+        schema,
+    }),
+    user: {
+        modelName: "userTable",
+    },
+    session: {
+        modelName: "sessionTable",
+    },
+    account: {
+        modelName: "accountTable",
+    },
 })
